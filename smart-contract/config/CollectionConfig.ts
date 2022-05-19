6b4e3c5f2691f36bf781db7d81ba8ad018d5a609
Ray Ortiz <oray@bgsu.edu>
2022-05-19 10:13:11 -0400

    init commit/project completed.

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.ethereumTestnet,
+  mainnet: Networks.ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'DragonEye',
+  tokenName: 'Dragon Eye',
+  tokenSymbol: 'DE',
+  hiddenMetadataUri: 'ipfs://QmcmJ1g8kNsiRRKUznLkjcigFk4sw7YZMy8dQ7W9axNNGN/hidden.json',
+  maxSupply: 10,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0x0755C2795E4Da86d34eef8d87233a36C894B4830",
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
